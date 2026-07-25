630a4337ccf73f6e1e64849c881c4f9c55b248c1
Saif Afridi 1251 <949saif@gmail.com>
2026-07-25 05:55:11 +0500

    add responsivense to Glass card

--- src/components/common/Glasscard.jsx
+++ src/components/common/Glasscard.jsx
@@ -2,15 +2,19 @@ function GlassCard({ children, className = "" }) {
   return (
     <div
       className={`
-        rounded-3xl
-        border border-white/10
+        relative
+        overflow-hidden
+        rounded-2xl
+        border
+        border-white/10
         bg-white/5
-        backdrop-blur-xl
-        shadow-2xl
+        backdrop-blur-lg
+        shadow-xl
         transition-all
         duration-300
-        hover:border-blue-500/40
+        hover:border-blue-500/30
         hover:shadow-blue-500/20
+        w-full
         ${className}
       `}
     >
